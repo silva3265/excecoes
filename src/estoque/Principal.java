60ab187ce829ae5bdbbcc8843933620cf6fd081d
silva3265 <silva3265@gmail.com>
2024-05-10 10:39:15 -0300

    Relançando exceções e printStackTrace

--- src/estoque/Principal.java
+++ src/estoque/Principal.java
@@ -4,33 +4,43 @@ import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		Produto produto = new Produto("Apple Watch");
-		produto.ativar();
-		produto.adicionarEstoque(20);
+    public static void main(String[] args) {
+        Produto produto = new Produto("Apple Watch");
+        produto.ativar();
+        produto.adicionarEstoque(20);
 
-		comprar(produto);
-	}
+        comprar(produto);
+    }
 
-	private static void comprar(Produto produto) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Quantidade: ");
-		int quantidade = scanner.nextInt();
+    private static void comprar(Produto produto) {
+        Scanner scanner = new Scanner(System.in);
 
-		efetuarBaixaEstoque(produto, quantidade);
-		System.out.println("Compra realizada");
-	}
+        do {
+            try {
+                System.out.print("Quantidade: ");
+                int quantidade = scanner.nextInt();
 
-	private static void efetuarBaixaEstoque(Produto produto, int quantidade) {
-		try { /* Tente executar esse bloco, o que vai colocar dentro do try vai depender da logica  */
-			produto.retirarEstoque(quantidade);
-			System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n", quantidade,
-					produto.getQuantidadeEstoque());
-		} catch (IllegalArgumentException iae) { /* caso for lan�ada a exception eu quero capiturar */
-			System.out.println("Erro ao efetuar baixa no estoque: " + iae.getMessage());
-		}
-		
-		/* IllegalArgumentException iae - tipo da exce��o, esse 'iae' � uma variavel */
-	}
+                efetuarBaixaEstoque(produto, quantidade);
+                System.out.println("Compra realizada");
+
+                break; /* se chegar a executar ele sai do while usando break*/
+            } catch (IllegalArgumentException iae) { /* caso ele saia ele printa na tela o erro */
+//                iae.printStackTrace(System.out);
+                iae.printStackTrace(); /* vai imprimir o erro no console */
+                System.out.println("Erro na compra: " + iae.getMessage());
+            }
+        } while (true); // Loop infinito, vai ficar retornando pro do
+    }
+
+    private static void efetuarBaixaEstoque(Produto produto, int quantidade) {
+//        try {
+            produto.retirarEstoque(quantidade);
+            System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n",
+                    quantidade, produto.getQuantidadeEstoque());
+//        } catch (IllegalArgumentException iae) {
+//            System.out.println("Erro ao efetuar baixa no estoque: " + iae.getMessage());
+//            throw iae;
+//        }
+    }
 
 }
