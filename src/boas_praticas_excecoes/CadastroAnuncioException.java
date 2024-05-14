2c1baebddfb90806db9296d2169c588c481e33b0
silva3265 <silva3265@gmail.com>
2024-05-14 15:52:01 -0300

    Boas práticas: não engula exceções

---
+++ src/boas_praticas_excecoes/CadastroAnuncioException.java
@@ -0,0 +1,9 @@
+package boas_praticas_excecoes;
+
+public class CadastroAnuncioException extends RuntimeException {
+
+    public CadastroAnuncioException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
